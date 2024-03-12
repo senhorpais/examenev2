7c5105abd275f828c877f38d5edeee0ec234424d
dammdcd2 <dammdcd2@a23eq12.sanclemente.local>
2024-03-12 20:25:21 +0100

    Commit inicial

---
+++ src/examen/GestiónCoche.java
@@ -0,0 +1,28 @@
+package examen;
+
+public class GestiónCoche {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Coche cocheDeMaria = new Coche ("Ford", "Focus",5000, 0);
+        Coche cocheDeMercedes = new Coche ("Audi", "A3", 34000,0);
+        
+        cocheDeMercedes.recorrerKm(43);
+        cocheDeMercedes.recorrerKm(240);
+        cocheDeMaria.recorrerKm(150);
+        cocheDeMercedes.recorrerKm(200);
+        cocheDeMaria.recorrerKm(65);
+        cocheDeMaria.recorrerKm(92);
+        System.out.println ("El km del cocheDeMaria es "+ cocheDeMaria.getKm()+"Km");
+        System.out.println ("El km del cocheDeMercedes es "+ cocheDeMercedes.getKm()+"Km");
+        System.out.println("El kilometraje total recorrido por los coches ha sido de " + Coche.getKmTotales() + "Km");
+        
+        //cocheDeMaria.aplicarDescuento();
+        //cocheDeMercedes.aplicarDescuento();
+        System.out.println ("El precio de venta del cocheDeMaria es "+ cocheDeMaria.precioConIva(true) +"€");
+        System.out.println ("El precio de venta del cocheDeMercedes es "+ cocheDeMercedes.precioConIva(false) +"€");
+    }
+    
+}
